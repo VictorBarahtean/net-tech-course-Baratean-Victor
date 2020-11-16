4a56d2eaf2e7ea8336100180e0aa3656eb8944a0
crm0157 <victor.barahtean@gmail.com>
2020-11-16 21:29:41 +0200

    implemented transfer

--- BlazorProject/Server/Controllers/WalletController.cs
+++ BlazorProject/Server/Controllers/WalletController.cs
@@ -6,8 +6,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using BlazorProject.Server.Models;
 using BlazorProject.Server.Data;
+using BlazorProject.Shared;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Wallet = BlazorProject.Server.Models.Wallet;
 
 namespace BlazorProject.Server.Controllers
 {
@@ -23,8 +25,8 @@ namespace BlazorProject.Server.Controllers
             this.context = context;
             this.userManager = userManager;
         }
-        
-        
+
+
         [HttpGet]
         public List<Wallet> GetWallets()
         {
@@ -34,6 +36,18 @@ namespace BlazorProject.Server.Controllers
             var wallets = context.Users.Include(x => x.Wallets).FirstOrDefault(x => x.Id == userId).Wallets;
             return wallets;
         }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Wallet GetWallet(Guid id)
+        {
+            //returneaza toate wallets
+            //var wallets = context.Wallets.ToList();
+            var userId = userManager.GetUserId(User);
+            var wallet = context.Users.Include(x => x.Wallets).FirstOrDefault(x => x.Id == userId).Wallets.FirstOrDefault(x => x.Id == id);
+            return wallet;
+        }
+
         [HttpPost]
         public IActionResult CreateWallet([FromQuery] string currency)
         {
@@ -58,15 +72,57 @@ namespace BlazorProject.Server.Controllers
 
             return Ok();
         }
+
         [HttpDelete]
-        public IActionResult DeleteWallet([FromQuery] Guid walletid)
+        [Route("{id}")]
+        public IActionResult DeleteWallet([FromRoute] Guid id)
         {
-            var wallet = context.Wallets.FirstOrDefault(x => x.Id == walletid);
+            var userId = userManager.GetUserId(User);
 
-            if (wallet == null) 
+            var user = context.Users.Include(x => x.Wallets).FirstOrDefault(x => x.Id == userId);
+
+            if (!user.Wallets.Any(x => x.Id == id))
+            {
                 return BadRequest();
-
+            }
+            var wallet = context.Wallets.Find(id);
             context.Wallets.Remove(wallet);
+            context.SaveChanges();
+
+            return Ok();
+        }
+        [HttpPost]
+        [Route("transfer")]
+        public ActionResult MakeTransfer([FromBody] TransferDTO data)
+        {
+            var userId = userManager.GetUserId(User);
+
+            var user = context.Users.Include(x => x.Wallets).FirstOrDefault(x => x.Id == userId);
+
+            if (!user.Wallets.Any(x => x.Id == Guid.Parse(data.SourceWalletId)))
+            {
+                return BadRequest();
+            }
+            var source = user.Wallets.FirstOrDefault(x => x.Id == Guid.Parse(data.SourceWalletId));
+            var destinationUser = context.Users.Include(x => x.Wallets).FirstOrDefault(x => x.UserName == data.Username);
+            var destination = destinationUser.Wallets.FirstOrDefault(x => x.Currency == data.Currency);
+
+            if(destination == null || source.Amount < data.Amount)
+            {
+                return BadRequest();
+            }
+            source.Amount -= data.Amount;
+            destination.Amount += data.Amount;
+
+            var transaction = new Transaction
+            {
+                Amount = data.Amount,
+                Date = DateTime.Now,
+                DestinationWalletId = destination.Id,
+                SourceWalletId = source.Id
+            };
+
+            context.Add(transaction);
             context.SaveChanges();
 
             return Ok();
---
+++ BlazorProject/Shared/TransferDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorProject.Shared
+{
+    public class TransferDTO
+    {
+        public string SourceWalletId { get; set; }
+        public string Username { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+    }
+}
