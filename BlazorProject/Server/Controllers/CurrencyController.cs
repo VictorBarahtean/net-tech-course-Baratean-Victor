efc229f221a40dd950702a2efea88d1ccef68c85
crm0157 <victor.barahtean@gmail.com>
2020-12-23 23:20:19 +0200

    *add selection currency

---
+++ BlazorProject/Server/Controllers/CurrencyController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorProject.Shared;
+
+namespace BlazorProject.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        [HttpGet]
+        public CurrencyList GetCurrencies()
+        {
+            return new CurrencyList
+            {
+                Currencies = new List<string>
+                {
+                    "EUR",
+                    "USD",
+                    "MDL",
+                    "EC"
+                }
+            };
+        }
+    }
+}
---
+++ BlazorProject/Shared/CurrencyList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorProject.Shared
+{
+    public class CurrencyList
+    {
+        public List<string> Currencies { get; set; }
+    }
+}
